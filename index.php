53c450c093cad33be93fb2073f3aaaa0c4ddc247
Jeferson Veiga <jeferson.veiiga@icloud.com>
2022-06-20 18:41:42 -0300

    Atualizando aula 13 Laços de repetição com for

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
 <?php
     /*
-    CONTEÚDO AULA 12
-    - Laços de repetição com while 
+    CONTEÚDO AULA 13
+    - Laços de repetição com for 
     */ 
-    $i = 1;
 
-    while($i < 11) {
+    for( $i = 1; $i < 11; $i++ ) {
+        if( $i == 2) {
+            continue; //Elimina da contagem o elemento selecionado
+        } else if( $i == 9 ) {
+            break; //Para o processo no elemento selecionado
+        }
         echo $i;
-        $i++;
     }
 
 ?>
